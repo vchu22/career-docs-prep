e2258b8eefcca44b8225ac9e58572a348c762ac2
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2024-08-03 01:09:29 +0000

    edit intro page and add new pages

---
+++ Home.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Career Docs Prep App",
+    page_icon="📄",
+)
+
+st.write("# 🏢 Career Docs Prep App 📄")
+
+st.sidebar.success("Select a tab above.")
+
+st.markdown(
+    """
+    A simple web app to help reduce mistakes when writing cover letters and resumes.
+    **👈 Select a tab from the sidebar** for the type of writing you are preparing for!
+    ## What docs are being covered?
+    ### Cover Letter 
+    - Working in progress 🚧
+    ### Resume
+    - Working in progress 🚧
+"""
+)
