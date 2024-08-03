a855019d8d149a9b40b61595976b67d8b02da267
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2024-08-03 16:40:09 +0000

    update home page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,23 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Career Docs Prep App",
+    page_title="Career Docs Prep Tools",
     page_icon="📄",
 )
 
-st.write("# 🏢 Career Docs Prep App 📄")
+st.write("# 🏢 Career Docs Prep Tools 📄")
 
 st.sidebar.success("Select a tab above.")
 
 st.markdown(
     """
-    A simple web app to help reduce mistakes when writing cover letters and resumes.
+    A simple web interface to help reduce mistakes when writing cover letters and resumes.
+    
     **👈 Select a tab from the sidebar** for the type of writing you are preparing for!
-    ## What docs are being covered?
+    ## What does this do?
     ### Cover Letter 
-    - Working in progress 🚧
+    - Autofill cover letter template with the job and the company you are applying for
+    - Replace the names on your previous cover letter with new ones
     ### Resume
     - Working in progress 🚧
 """
