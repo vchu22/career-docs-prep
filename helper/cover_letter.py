e01fe8b18ce6b4d849b4759333da9d5d316f6cd2
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2024-08-03 10:09:20 +0000

    organize search and replace terms using dictionary

--- helper/cover_letter.py
+++ helper/cover_letter.py
@@ -1,3 +1,5 @@
+import re
+
 class CoverLetter:
     def __init__(self) -> None:
         self.company_name = ""
@@ -5,10 +7,16 @@ class CoverLetter:
         self.referer = ""
         self.applicant_name = ""
         self.template = ""
-
+    
     def fill_template(self) -> str:
-        txt = self.template.replace("[Company Name]", self.company_name)
-        txt = txt.replace("[Job Title]", self.job_title)
-        txt = txt.replace("[Referral Source]", self.referer)
-        txt = txt.replace("[Applicant Name]", self.applicant_name)
+        txt = self.template
+        rep = {"[Company Name]": self.company_name, 
+               "[Job Title]": self.job_title,
+               "[Referral Source]": self.referer,
+               "[Applicant Name]": self.applicant_name}
+
+        # replacement
+        rep = dict((re.escape(k), v) for k, v in rep.items()) 
+        pattern = re.compile("|".join(rep.keys()))
+        txt = pattern.sub(lambda m: rep[re.escape(m.group(0))], txt)
         return txt
